2a776f38daca8091ae3dbdeb64feabe7b97806fb
Shrey <sb15895@my.bristol.ac.uk>
2023-02-06 16:15:50 +0000

    data retrival functions complete

--- run_dir/visuals/utils.py
+++ run_dir/visuals/utils.py
@@ -45,55 +45,113 @@ def HPCG_iocomp_timers(parentDir):
     """
     Iterate over core sizes
     """
-    data_io = {} # dictionary for I/O stuff 
     for core_size in dir_list:
         core = core_size.split("_",1)[1] # hardcoded value of only 1 node used 
-        layer_list = next(os.walk(f"{parentDir}/{core_size}"))[1]
-        
+
         """
-        Iterate over I/O layers 
+        Iterate over array sizes 
         """
-        for ioLayer in layer_list: 
+        data_array = {} 
+        array_list = next(os.walk(f"{parentDir}/{core_size}"))[1]
+        for arraySize in array_list: 
 
-            filename = f"{parentDir}/{core_size}/{ioLayer}/iocomp_timers.txt"
-
-            """
-            read info from text file 
-            and convert into PD dictionary 
-            """ 
-            mydata = pd.read_csv(filename,index_col=False,skiprows=0) 
+            layer_list = next(os.walk(f"{parentDir}/{core_size}/{arraySize}"))[1]
 
             """
-            average times 
+            Iterate over I/O layers 
             """
-            avg = {} 
-            avg = average_function(mydata) 
+            data_io = {} # dictionary for I/O stuff 
+            for ioLayer in layer_list: 
 
+                """
+                Iterate over slurm mappings 
+                """
+                data_mapping = {} 
+                slurm_list = next(os.walk(f"{parentDir}/{core_size}/{arraySize}/{ioLayer}"))[1]
+                for slurmMapping in slurm_list: 
+
+                    """
+                    Iterate over job arrays 
+                    """
+                    avg = {} 
+                    for jobIter in range(3): 
+                        
+                        filename = f"{parentDir}/{core_size}/{arraySize}/{ioLayer}/{slurmMapping}/{jobIter}/iocomp_timers.txt"
+
+                        """
+                        read info from text file 
+                        and convert into PD dictionary 
+                        """ 
+                        mydata = pd.read_csv(filename,index_col=False,skiprows=0) 
+
+                        """
+                        average times 
+                        """
+                        avg[jobIter] = average_function(mydata)
+                    
+                    """
+                    Average over job array
+                    add data per slurm mapping
+                    """
+                    data_mapping[slurmMapping] = average_jobs(avg)
+
+                """
+                add data per I/O layer
+                """
+                data_io[ioLayer] = data_mapping
+            
             """
-            add data per I/O layer
+            add data per array size 
             """
-            data_io[filename] = avg 
-        
+            data_array[arraySize] = data_io
+
         """
         add data per core size 
         """
-        data[core] = data_io
-
+        data[core] = data_array
+    
     print(data)
 
-    
 
+    
+"""
+average_function averages loop, wait and comp time per file 
+"""
 def average_function(mydata): 
 
     loopTime_avg = mydata['loopTime(s)'].mean() 
     waitTime_avg = mydata[' waitTime(s)'].mean() 
     compTime_avg = mydata['compTime(s)'].mean() 
     avg = {}
-    avg[loopTime_avg] = loopTime_avg
-    avg[waitTime_avg] = waitTime_avg
-    avg[compTime_avg] = compTime_avg
+    avg["loopTime_avg"] = loopTime_avg
+    avg["waitTime_avg"] = waitTime_avg
+    avg["compTime_avg"] = compTime_avg
     return(avg) 
 
+"""
+average_jobs averages the average loop, wait and comp time over the array of jobs
+"""
+
+def average_jobs(avg): 
+
+    loopTime_avg = 0 
+    waitTime_avg = 0 
+    compTime_avg = 0 
+
+    avg_job = {} 
+
+    for key, value in avg.items():
+        # key will be 0,1,2 etc job array submissions
+        
+        loopTime_avg += avg[key]["loopTime_avg"] 
+        waitTime_avg += avg[key]["waitTime_avg"] 
+        compTime_avg += avg[key]["compTime_avg"] 
+    
+    avg_job["loopTime"] = loopTime_avg/3
+    avg_job["waitTime"] = waitTime_avg/3
+    avg_job["compTime"] = compTime_avg/3
+
+    return(avg_job)
 
 
 
