5a292cd374dcbfb268cadb6939ada527cc014e7d
sb15895 <s2032563@ed.ac.uk>
2023-05-01 22:27:25 +0100

    updated slurm files, nx, ny, nz initialised using srun

--- run_dir/slurm_files/consecutive.sh
+++ run_dir/slurm_files/consecutive.sh
@@ -14,10 +14,10 @@ bar=$(IFS=, ; echo "${vals[*]}")
 
 TOTAL_RANKS=$((${SLURM_NNODES} * ${SLURM_NTASKS_PER_NODE} ))
 if (($MAP == 1)); then 
-  map -n ${TOTAL_RANKS} --mpiargs="--hint=nomultithread  --distribution=block:block  --nodes=${SLURM_NNODES} --cpu-bind=map_cpu:${bar[@]}" --profile  ${HPCG} --nx=${SIZE} --ny=${SIZE} --nz=${SIZE} --io=${m} --HT=1
+  map -n ${TOTAL_RANKS} --mpiargs="--hint=nomultithread  --distribution=block:block  --nodes=${SLURM_NNODES} --cpu-bind=map_cpu:${bar[@]}" --profile  ${HPCG} --nx=${NX} --ny=${NY} --nz=${NZ} --io=${m} --HT=1
 
 else 
-  srun  --hint=nomultithread  --distribution=block:block  --nodes=${SLURM_NNODES} --cpu-bind=map_cpu:${bar[@]} ${HPCG} --nx=${SIZE} --ny=${SIZE} --nz=${SIZE} --io=${m} --HT=1 > test.out
+  srun  --hint=nomultithread  --distribution=block:block  --nodes=${SLURM_NNODES} --cpu-bind=map_cpu:${bar[@]} ${HPCG} --nx=${NX} --ny=${NY} --nz=${NZ} --io=${m} --HT=1 > test.out
 fi 
 
 echo "JOB ID"  $SLURM_JOBID >> test.out
