4f3a3ce3a37f4b76068705ce9b8da2f399729a3a
Shrey <sb15895@my.bristol.ac.uk>
2023-10-30 15:19:38 +0000

    updated gitignore, deleted lots of files and added run script

--- build/testing/run.sh
+++ build/testing/run.sh
@@ -1,7 +1,11 @@
-rm *.btr 
-rm *.txt
+# remove HPCG output text files 
+rm *.txt 
+# remove I/O output files
 rm *.h5 
-rm *.dat
-# mpirun.mpich -n 2 ../bin/xhpcg --nx=16 --ny=16 --nz=16 --io=1 --sh=0
-# mpirun.mpich -n 2 ../bin/xhpcg --nx=16 --ny=16 --nz=16 --io=1 --sh=1
-mpirun.mpich -n 2 ../bin/xhpcg --nx=16 --ny=16 --nz=16 --io=1 --HT=1
+rm *.dat 
+rm -rf x_*
+# remove btr files
+rm *.btr
+# run code 
+mpirun.mpich -n 2 ../bin/xhpcg --nx=16 --ny=16 --nz=16 --io=3 --sh=1
+
