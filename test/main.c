5bc454bd71b2273d5c5829cca84b14d30ed06016
Shrey <sb15895@my.bristol.ac.uk>
2023-11-07 13:21:15 +0000

    added test functions for hpcg

---
+++ test/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>  
+#include <string.h> 
+#include <assert.h> 
+#include <math.h>
+#include <adios2_c.h>
+#include "test.h"
+#define CONFIG_FILE_ADIOS2 "config.xml"
+
+// small fn to check if file exists
+#include <sys/stat.h>   // stat
+#include <stdbool.h>    // bool type
+bool file_exists (char *filename) {
+  struct stat   buffer;   
+  return (stat (filename, &buffer) == 0);
+}
+
+int main(int argc, char** argv)
+{
+	int ierr;
+	ierr = MPI_Init(&argc, &argv);  
+	mpi_error_check(ierr); 
+
+	/* 
+	 * initialise structs 
+	 */ 
+	struct iocomp_params iocompParams; 
+	struct test_params testParams; 
+
+	/*	
+	 * initialise stream param structs using command line parameters
+	 */ 
+	arguments(&testParams, argc, argv); 
+	testParams.localDataSize = testParams.nx * testParams.ny;
+
+	MPI_Comm comm = MPI_COMM_WORLD; 
+	int rank; 
+	int size; 
+	MPI_Comm_rank(comm, &rank); 
+	MPI_Comm_size(comm, &size); 
+	// data parameters definitions 
+
+	if(!rank){
+		printf("Testing for HPCG benchmark output started. \n"); 
+		printf("Size of array %i x %i IO num %i \n", testParams.nx, testParams.ny, testParams.io); 
+	} 
+
+	/*
+	 * iocomp - iocompInit initialises the ioServer 
+	 * and initialises the compute comm 
+	 * HT flag and shared flag have been turned off. 
+	 * Compute Comm is the same as WORLD COMM  
+	 */ 
+	MPI_Comm computeComm = iocompInit(&iocompParams,comm, 0, testParams.io, NODESIZE, 0, NUMWIN); 
+	ioServerInitialise(&iocompParams);
+  iocompParams.localDataSize = testParams.localDataSize; 
+	arrayParamsInit(&iocompParams, comm); 
+	
+	// first set of tests: file is read, values are checked. 
+	HPCG_tests(&testParams, &iocompParams,  MPI_COMM_WORLD); 
+	
+	MPI_Finalize(); 
+	if(!rank){
+		printf("Testing ends\n"); 
+	} 
+} 
+
---
+++ test/test.h
@@ -0,0 +1,40 @@
+#include <mpi.h>
+#include <stdbool.h>
+#include <adios2_c.h>
+#include <stdio.h>
+#include <stdlib.h>  
+#include <string.h> 
+#include "iocomp.h"
+
+#define KERNELS 1 
+#define AVGLOOPCOUNT 10
+#define NODESIZE 128
+#define NUMWIN 1 
+
+// test parameters structure 
+struct test_params{
+	int LOOP_COUNT; 
+	// sizes
+	size_t localDataSize; 
+	size_t globalDataSize; 
+	char* fullResults_filename[KERNELS]; 
+	int writeFreq; // compute steps per writing
+	int numWrites; // max number of writes  
+
+	// command line args 
+	bool HT_flag, sharedFlag, testFlag, verbose; 
+	int nx, ny, io; 
+
+	// file object for debug 
+	char debugFile[100]; 
+	FILE* debug; 
+}; 
+extern struct test_params testParams; 
+
+void arguments(struct test_params* testParams, int argc, char** argv); 
+int valueCheck(struct iocomp_params *iocompParams, double* iodata_test, double val, char* filename); 
+double* readFiles(struct test_params *testParams, struct iocomp_params *iocompParams, char* readFile); 
+void HPCG_tests(struct test_params *testParams, struct iocomp_params *iocompParams,  MPI_Comm comm); 
+bool file_exists (char *filename); 
+void deleteFilesTest(char* fileName); 
+
