1ff6a8f857707f2ac6516e0e42585d8385cde60c
sb15895 <s2032563@ed.ac.uk>
2024-03-21 17:00:47 +0000

    changes to run stuff

--- run_dir/runscript.sh
+++ run_dir/runscript.sh
@@ -6,9 +6,9 @@ callWeakScaling () {
     export NY=$((2**7)) 
     export NZ=$((2**7)) 
     export NODE_START=0
-    export NODE_END=3
-    export IO_START=0
-    export IO_END=3
+    export NODE_END=4
+    export IO_START=2
+    export IO_END=2
     export ARRAY="0-2"
     export TIME="04:00:00"
     export CASE_START=0
@@ -44,51 +44,48 @@ callTest () {
     export NX=$((2**5))
     export NY=$((2**5)) 
     export NZ=$((2**5)) 
-    #export NX=$((2**6))
-    #export NY=$((2**7)) 
-    #export NZ=$((2**7)) 
-    export NODE_START=0
-    export NODE_END=1
+    export NODE_START=3
+    export NODE_END=3
     export IO_START=0
     export IO_END=0
     export ARRAY="0"
-    export TIME="00:10:00"
-    export DIR=$(pwd)/TEST 
-    export MAP=0
-    export WAIT=""
+    export TIME="00:05:00"
+    export MAP=1
+    export WAIT="--wait"
     export DARSHAN=0
-    sh ./weakScaling.sh
+    export DIR=$(pwd)/TEST/ARM
+    for CASE in $(seq 0 4)
+    do 
+      export CASE_START=${CASE}
+      export CASE_END=${CASE}
+      sh ./weakScaling.sh
+    done 
   )
 } 
 
 callMAP() {
   (
     export PPN=128
-    export NX=$((2**5))
-    export NY=$((2**4)) 
-    export NZ=$((2**5)) 
-    export NODE_START=4
-    export NODE_END=4
+    export NX=$((2**6))
+    export NY=$((2**7)) 
+    export NZ=$((2**7)) 
+    export NODE_START=3
+    export NODE_END=3
+    export IO_START=0
+    export IO_END=0
     export ARRAY="0"
-    export TIME="00:20:00"
-    export DIR=MAP_PROFILES/STRONG # or weak 
+    export TIME="00:30:00"
     export MAP=1
-    # loop over IO layers for MAP from 0 to 3 
-    
-    for io in 0 1 3 
-    # for io in $(seq 3 3)
+    export WAIT="--wait"
+    export DARSHAN=0
+    export DIR=$(pwd)/PROFILING/ARM_MAP/v2.1.0
+    for CASE in $(seq 0 0)
     do 
-      export IO_START=${io}
-      export IO_END=${io} 
-      # loop over cases 1 by 1 for MAP # from 1 - 5 
-      for case in $(seq 1 5)
-      do 
-        export CASE_START=${case}
-        export CASE_END=${case}
-        sh ./weakScaling.sh
-      done 
+      export CASE_START=${CASE}
+      export CASE_END=${CASE}
+      sh ./weakScaling.sh
     done 
-  ) 
+  )
 }
 
 # Command line arguments 
